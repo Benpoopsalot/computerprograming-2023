376612d4640c4727265666ffd624ec159ab5bcab
XL <111790917+Benpoopsalot@users.noreply.github.com>
2023-02-27 11:43:14 -0700

    Add files via upload

---
+++ src/Pig Latin/main.py
@@ -0,0 +1,4 @@
+count = 99
+while(count>0):
+  print(count,"bottles of coom on the wall", count, "bottles of coom, take one down and pass it around,",count-1, "bottles of coom on the wall")
+  count = count-1
